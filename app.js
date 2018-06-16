3335997a4812541def16dd6f50bc8e3a886698ad
fengqi <fengqi_bj@ysstech.com>
2018-06-16 22:48:23 +0800

    静态文件

---
+++ app.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let app = express();
+
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
+// app.use(express.static('public'));
+app.use('/static', express.static('public'));
+
+
+let server = app.listen(3000, function() {
+  let host = server.address().address;
+  let port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
